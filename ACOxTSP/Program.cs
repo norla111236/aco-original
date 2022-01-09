219dab49baae54155ba0a6fecb50e2e6ac6f67a2
norlaChien <79412741+norla111236@users.noreply.github.com>
2022-01-09 23:01:05 +0800

    Add files via upload

---
+++ ACOxTSP/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Metaheuristic;//LAB Library
+
+namespace ACOxTSP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int RunLength=2/*執行次數*/;
+            
+            TSP aco = new TSP();
+
+            System.IO.StreamWriter sw = new System.IO.StreamWriter("OutPut.csv");//寫入檔案
+          
+            sw.WriteLine("Case" + "," + "Best"/*BestSolution*/+ "," + "AVG" + "," + "SD" + "," + "AVG Time");//寫入抬頭
+
+            for (int problem = 0; problem < aco.problem.Length; problem++)
+            {
+                double[] BestFitness = new double[RunLength];/*存放每次執行的最佳解*/
+                double BFSum = 0.0,BestOfEachRun = double.MaxValue;/*最小化問題*/
+
+                System.Diagnostics.Stopwatch swTimer = new System.Diagnostics.Stopwatch();//計時器(單位:毫秒)
+
+                aco.InitialProblem(problem);
+
+                Console.WriteLine("Problem "+(problem+1) +" "+aco.problem[problem]);
+
+                for (int run = 0; run < RunLength; run++)
+                {
+                    swTimer.Reset();/*計時器歸零*/
+                    swTimer.Start();/*計時器開始*/
+                   
+                    aco.Init(40,aco.NumOfCity,0,aco.NumOfCity - 1,ACOOption.RepeatableOption.Nonrepeatable,ACOOption.CycleOption.Cycle);
+                    aco.Run(4000,1,5,0.8,10);
+                    //aco.Run(4000, 1, 5, 0.9, 0, 0.8, 1);  //local pheromone and global pheromone
+
+                    BestFitness[run] = aco.GbestFitness;
+                    BFSum += BestFitness[run];
+
+                    if (BestFitness[run] < BestOfEachRun)
+                    {/*找出每次RUN的最佳解中最好的那個*/
+                        BestOfEachRun = BestFitness[run];
+                    }
+
+                    Console.WriteLine("No." + (run+1) +" Run : "+ "Best Finess = " + BestFitness[run]);
+
+                }
+                swTimer.Stop();/*計時器暫停*/
+                double TimeCosts = swTimer.Elapsed.TotalMilliseconds / 1000.0;/*毫秒轉秒*/
+
+                Console.WriteLine(aco.problem[problem] + "," + BestOfEachRun + "," + (BFSum / RunLength) + "," + SD(BestFitness) + "," + TimeCosts / RunLength);//印出
+                sw.WriteLine(aco.problem[problem] + "," + BestOfEachRun + "," + (BFSum / RunLength) + "," + SD(BestFitness) + "," + TimeCosts / RunLength);//儲存
+            }
+            sw.Close();
+            Console.Read();
+        }
+        public static double SD(double[] fit)
+        {
+            double sum = 0.0, average;
+            for (int i = 0; i < fit.Length; i++)
+                sum += fit[i];
+            average = sum / fit.Length;
+            sum = 0.0;
+            for (int i = 0; i < fit.Length; i++)
+                sum += (Math.Pow(fit[i] - average, 2));
+            return Math.Pow(sum / fit.Length, 0.5);
+        }
+    }
+}
---
+++ ACOxTSP/TSP.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Metaheuristic;
+
+namespace ACOxTSP
+{
+    class TSP : ACO
+    {
+        public string[] problem = new string[] { "Bays29", "Berlin52", "Eil51", "Eil76", "Pr76", "St70", "Oliver30" };
+
+        public double[][] Distance;
+        public int NumOfCity;
+
+        #region 載入TSP
+
+        public void InitialProblem(int problemIndex)
+        {/*根據TSP問題的資料不同，有不同的初始化方式*/
+            /*
+             * 目前實驗的問題都是完整且對稱的TSP問題
+             * 完整就是每個城市都能到達除了自己以外的所有城鎮。
+             * 對稱就是N城鎮到M城鎮的距離 = M城鎮到N城鎮的距離
+             */
+
+            string FileLoad = "../../TSP/" + problem[problemIndex]+".txt";
+
+            #region 抓取城市數量
+            string CatchNumOfCity = "";
+            foreach (char c in problem[problemIndex])
+            {/*抓字串中的數字部分*/
+                if ((int)c >= 48 && (int)c <= 57)
+                    CatchNumOfCity += c;
+            }
+            NumOfCity = int.Parse(CatchNumOfCity);
+            #endregion
+
+            #region 距離矩陣資料
+            if (problemIndex == 0)
+            {/*已經算好距離矩陣的二維資料->直接讀取*/
+
+                Distance = new double[NumOfCity][];
+                for (int city = 0; city < NumOfCity; city++)
+                    Distance[city] = new double[NumOfCity];
+                readStreetDistance(FileLoad);
+            }
+            #endregion
+
+            #region 座標位置資料
+            else
+            {/*僅給予各城市之座標，自行計算二維距離矩陣*/
+                readGeographicalDistance(FileLoad);
+            }
+            #endregion
+        }
+        
+        public void readGeographicalDistance(string file)
+        {
+            System.IO.StreamReader str = new System.IO.StreamReader(file);
+            string all = str.ReadToEnd();
+            string[] c = all.Split(new char[] { ' ', '\n' });
+            Distance = new double[int.Parse(c[0])][];
+            for (int i = 0; i < int.Parse(c[0]); i++)
+                Distance[i] = new double[int.Parse(c[0])];
+            for (int i = 0; i < int.Parse(c[0]); i++)
+            {
+                for (int j = 0; j < int.Parse(c[0]); j++)
+                {
+                    int t1 = 3 * i + 1, t2 = 3 * j + 1;
+                    double x1 = Double.Parse(c[t1 + 1]), y1 = Double.Parse(c[t1 + 2]);
+                    double x2 = Double.Parse(c[t2 + 1]), y2 = Double.Parse(c[t2 + 2]);
+                    Distance[i][j] = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+                }
+            }
+        }
+        public void readStreetDistance(string file)
+        {
+            System.IO.StreamReader str = new System.IO.StreamReader(file);
+            string all = str.ReadToEnd();
+            string[] c = all.Split(new char[] { ' ', '\n' });
+            int index = 0;
+            for (int i = 0; i < NumOfCity; i++)
+            {
+                for (int j = 0; j < NumOfCity; j++)
+                {
+                    for (int k = index; k < c.Length; k++)
+                    {
+                        if (c[k] != "")
+                        {
+                            index = k + 1;
+                            break;
+                        }
+                    }
+                    Distance[i][j] = Double.Parse(c[index - 1]);
+                }
+            }
+        }
+
+        #endregion
+
+        public override void InitVisibility()
+        {//初始化Library之中的Visibility , Library初始化時，會自動呼叫此函式執行，所以不要執行這部分。
+            for (int i = 0; i < Visibility.GetLength(0); i++)
+                for (int j = 0; j < Visibility.GetLength(1); j++)
+                    Visibility[i, j] = 1 / Distance[i][j];
+        }
+
+        public override double Fitness(int[] solution)
+        {
+            double sum = 0;
+            for (int j = 0; j < solution.GetLength(0) - 1; j++)
+                sum = sum + Distance[solution[j]][solution[j + 1]];
+            sum = sum + Distance[solution[solution.GetLength(0) - 1]][solution[0]];
+
+            return sum;
+        }
+    }
+}
